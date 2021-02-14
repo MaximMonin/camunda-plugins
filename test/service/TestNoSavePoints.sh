31412a054566dddbe311a59a83a542adea91f6ad
Maxim Monin <maximmonin@gmail.com>
2021-02-14 18:44:17 +0000

    stress test tuning

--- test/service/TestNoSavePoints.sh
+++ test/service/TestNoSavePoints.sh
@@ -4,7 +4,7 @@ set -e
 source ../../.env
 auth='-u camunda:'$(echo $CAMUNDA_PASSWORD | base64 --decode -w0)
 
-for (( i=0; i<100; ++i)); do
+for (( i=0; i<1000; ++i)); do
   curl -k -H "Content-Type: application/json" -X POST \
     -d '{"variables": {"withSavePoints": {"value": "false"} } }' \
     https://localhost:$CLUSTER_GATE_PORT/engine-rest/process-definition/key/Service.EngineSpeedTest/start $auth
--- test/service/stress2.sh
+++ test/service/stress2.sh
@@ -7,6 +7,6 @@ auth=$(echo -n $user | base64)
 docker pull williamyeh/wrk
 
 myip=$(hostname -I | cut -d' ' -f1)
-# run 100 connections within 10 threads during 10 seconds (spam start process)
+# run 100 connections within 10 threads during 1 second (spam start process)
 docker run --rm -v `pwd`:/data --net camunda-plugins-$ENVIRONMENT williamyeh/wrk -s stress2.lua -t10 -c100 -d10s -H "Authorization: Basic $auth" \
   https://$myip:$CLUSTER_GATE_PORT/engine-rest/process-definition/key/Service.EngineSpeedTest/start
