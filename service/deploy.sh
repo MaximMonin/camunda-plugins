f11a87c29ed5e050b53634abc1cb748533019a28
Maxim Monin <maximmonin@gmail.com>
2021-04-07 16:05:22 +0300

    models use telegramChannel DMN table

--- service/deploy.sh
+++ service/deploy.sh
@@ -13,3 +13,11 @@ for filename in service/*.bpmn; do
    curl -k -H "Content-Type: multipart/form-data" -X POST -F $upload -F $deployname -F 'deploy-changed-only=true' \
       https://localhost:$CLUSTER_GATE_PORT/engine-rest/deployment/create $auth
 done
+
+for filename in service/*.dmn; do
+   model=$(cat $filename |grep '  <decision id="' | cut -d'"' -f 2)
+   upload='upload=@"./'$filename'"'
+   deployname='deployment-name='$model
+   curl -k -H "Content-Type: multipart/form-data" -X POST -F $upload -F $deployname -F 'deploy-changed-only=true' \
+      https://localhost:$CLUSTER_GATE_PORT/engine-rest/deployment/create $auth
+done
--- ssl.sh
+++ ssl.sh
@@ -10,6 +10,6 @@ openssl req -new -newkey rsa:2048 -nodes -out cfg/ssl/camunda.csr -keyout cfg/ss
 openssl x509 -req -days 3650 -in cfg/ssl/camunda.csr -CA cfg/ssl/rootcamundaCA.crt -CAkey cfg/ssl/rootcamundaCA.key -CAcreateserial -out cfg/ssl/camunda.crt -extfile cfg/v3.cnf
 
 openssl pkcs12 -export -in cfg/ssl/camunda.crt -inkey cfg/ssl/camunda.key -out cfg/ssl/camunda.p12 -name tomcat
-openssl dhparam -out cfg/ssl/dhparam.pem 4096
+openssl dhparam -dsaparam -out cfg/ssl/dhparam.pem 4096
 chmod 644 cfg/ssl/camunda.p12
 
