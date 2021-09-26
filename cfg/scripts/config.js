319d0be568dfff1ecbca491ef2d1e8c4ba1e2198
Maxim Monin <maximmonin@gmail.com>
2021-09-26 12:19:56 +0000

    History plugin update from source

--- cfg/scripts/config.js
+++ cfg/scripts/config.js
@@ -20,10 +20,11 @@ export default {
     name: 'Cockpit',
     vendor: 'Camunda'
   },
-   customScripts: [
+  customScripts: [
     'scripts/definition-historic-activities.js',
     'scripts/instance-historic-activities.js',
-    'scripts/instance-route-history.js'
+    'scripts/instance-route-history.js',
+    'scripts/tasklist-audit-log.js'
   //  'scripts/instance-tab-modify.js'
   ],
   // requireJsConfig: {
@@ -68,18 +69,19 @@ export default {
   //   // select if Historic Batches should be loaded automatically when navigating to #/batch
   //   'autoLoadEnded': true
   // },
-  // bpmnJs: {
+     bpmnJs: {
   //   moddleExtensions: {
   //     // if you have a folder called 'my-custom-moddle' (in the 'cockpit' folder)
   //     // with a file called 'camunda.json' in it defining the 'camunda' moddle extension
   //     camunda: 'my-custom-moddle/camunda'
   //   },
-  //   additionalModules: [
+       additionalModules: [
   //     // if you have a folder called 'my-custom-module' (in the 'cockpit' folder)
   //     // with a file called 'module.js' in it
   //     'my-custom-module/module'
-  //   ],
-  // },
+         'scripts/robot-module.js'
+       ],
+     },
   // defaultFilter: {
   //   historicProcessDefinitionInstancesSearch: {
   //     lastDays: 5,
@@ -87,7 +89,7 @@ export default {
   //   }
   // },
   // csrfCookieName: 'XSRF-TOKEN',
-  disableWelcomeMessage: true,
+     disableWelcomeMessage: true,
   // userOperationLogAnnotationLength: 5000,
-  // previewHtml: true
+     previewHtml: true
 };
