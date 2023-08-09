2fcb7c3bfa9321e6113eba235b09b223356044be
Maxim Monin <maximmonin@gmail.com>
2023-08-09 04:13:47 +0000

    deploy script compatible with Camunda Modeller

--- service/deploy.sh
+++ service/deploy.sh
@@ -7,17 +7,23 @@ source ../.env
 auth='-u camunda:'$(echo $CAMUNDA_PASSWORD | base64 --decode -w0)
 
 for filename in service/*.bpmn; do
-   model=$(cat $filename |grep '  <bpmn:process id="' | cut -d'"' -f 2)
+   model=$(cat $filename | grep '  <bpmn:process id="' | cut -d'"' -f 2)
    upload='upload=@"./'$filename'"'
-   deployname='deployment-name='$model
+   fileshort=$(echo $filename | cut -d'.' -f 1 | cut -d'/' -f 2)
+   deployname='deployment-name='$fileshort
+   echo $model' -> '$fileshort
    curl -k -H "Content-Type: multipart/form-data" -X POST -F $upload -F $deployname -F 'deploy-changed-only=true' \
-      https://localhost:$CLUSTER_GATE_PORT/engine-rest/deployment/create $auth
+      -F 'deployment-source=Camunda Modeler' https://localhost:$CLUSTER_GATE_PORT/engine-rest/deployment/create $auth
+   echo ' done'
 done
 
 for filename in service/*.dmn; do
-   model=$(cat $filename |grep '  <decision id="' | cut -d'"' -f 2)
+   model=$(cat $filename | grep '  <decision id="' | cut -d'"' -f 2)
    upload='upload=@"./'$filename'"'
-   deployname='deployment-name='$model
+   fileshort=$(echo $filename | cut -d'.' -f 1 | cut -d'/' -f 2)
+   deployname='deployment-name='$fileshort
+   echo $model' -> '$fileshort
    curl -k -H "Content-Type: multipart/form-data" -X POST -F $upload -F $deployname -F 'deploy-changed-only=true' \
-      https://localhost:$CLUSTER_GATE_PORT/engine-rest/deployment/create $auth
+      -F 'deployment-source=Camunda Modeler' https://localhost:$CLUSTER_GATE_PORT/engine-rest/deployment/create $auth
+   echo ' done'
 done
